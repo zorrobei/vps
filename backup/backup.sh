eec8dfbbaeafd4f0b039701e10bd82adb57a05b2
Zorro Bei <hzbeiyx@gmail.com>
2017-12-06 00:02:46 +0800

    Saving the files through bzip2

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+# Program:
+#	The program saves the necessary data of your server.
+# History:
+# 2017/12/04	Zorro Bei	v0.0.1
+#	First release.
+# 2017/12/05	Zorro Bei	v0.0.2
+#	Saving the files through bzip2.
+#	Don't log files saved as default. If you want to save the log, use the parameter log.
 
-if [ "${1}" == "log" ]; then
-	logger -p syslog.info "backup.sh is starting..."
-fi
+PATH=/bin:/sbin;/usr/bin;/usr/sbin:/usr/local/bin:/usr/local/sbin
+export PATH
 
+basedir=/backups
 source="/etc /home /root /var/lib /var/spool/{cron,at,mail}"
-target="/backups/backup-system-$(date +%Y-%m-%d).tar.gz"
-[ ! -d /backups ] && mkdir /backups
-tar -zcvf ${target} ${source} &> /backups/backup.log
+target="${basedir}/backup-data-$(date +%Y-%m-%d).tar.gz"
+
 if [ "${1}" == "log" ]; then
-	logger -p syslog.info "backup.sh is finished."
+	tar -cjpvf ${target} ${source} &> ${basedir}/backup.log
+else
+	tar -cjpf ${target} ${source}
 fi
