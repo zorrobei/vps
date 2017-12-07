1c882bf67f477e5da31ecea9e8089190ebeff06a
Zorro Bei <hzbeiyx@gmail.com>
2017-12-07 11:54:44 +0800

    Checking whether basedir exists. If not create the directionary.

--- backup/backup.sh
+++ backup/backup.sh
@@ -7,6 +7,8 @@
 # 2017/12/05	Zorro Bei	v0.0.2
 #	Saving the files through bzip2.
 #	Don't log files saved as default. If you want to save the log, use the parameter log.
+# 2017/12/07	Zorro Bei	V0.0.3
+#	Checking whether basedir exists. If not create the directionary.
 
 PATH=/bin:/sbin;/usr/bin;/usr/sbin:/usr/local/bin:/usr/local/sbin
 export PATH
@@ -14,6 +16,8 @@ export PATH
 basedir=/backups
 source="/etc /home /root /var/lib /var/spool/{cron,at,mail}"
 target="${basedir}/backup-data-$(date +%Y-%m-%d).tar.gz"
+
+[ -d /backups ] || mkdir /backups
 
 if [ "${1}" == "log" ]; then
 	tar -cjpvf ${target} ${source} &> ${basedir}/backup.log
